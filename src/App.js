afbd6bd00fe50b0d7d0efacc1d87616b992ee366
semen-zharkov <semen.zharkov.2017@yandex.ru>
2024-02-26 22:20:06 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    axios
+    .get('http://127.0.0.1:8000')
+    .then(response => console.log(response.data))
+    .catch(error => console.error(error));
+}, []);
+
+return (
+  <div>
+
+  </div>
+);
 }
 
 export default App;
