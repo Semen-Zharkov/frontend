3e933248177a4796b0146c7e2b8dc0f157594955
Kostya Pershin <kostya.pershin.18@mail.ru>
2024-03-14 00:01:47 +0500

    user get data update

--- src/components/authorization/formLogIn/FormLogIn.jsx
+++ src/components/authorization/formLogIn/FormLogIn.jsx
@@ -7,7 +7,7 @@ function FormLogIn(){
     const [token, setToken] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    
+
     const{
         register,
         formState:{errors,
@@ -15,56 +15,61 @@ function FormLogIn(){
         handleSubmit,
     } = useForm();
 
-    const onSubmit = async () => {
-        try {
-        fetch('http://127.0.0.1:8000/auth/login',{
+    const onSubmit = async (data) => {
+    // Отправка запроса на вход
+    fetch('https://553f-5-165-8-39.ngrok-free.app/auth/login',{
         method: 'POST',
+        credentials: 'include',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
-        body: JSON.stringify({ username, password }),
+        body: new URLSearchParams({
+            'grant_type': '',
+            'username': data['username'],
+            'password': data['password'],
+            'scope': '',
+            'client_id': '',
+            'client_secret': '',
         })
-        if (Response.ok) {
-            // Если вход прошел успешно, получаем токен из ответа
-            const data = await Response.json();
-            const token = data.token;
-    
-            // Сохраняем токен в локальное хранилище
-            localStorage.setItem('token', token);
-    
-            // Обновляем состояние компонента с токеном
-            setToken(token);
-    
-            // Получаем данные пользователя
-            const userDataResponse = await fetch('http://127.0.0.1:8000/user/me', {
-              method: 'Get',
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            });
-            const userData = await userDataResponse.json();
-    
-            // Здесь можно выполнить какие-то действия с данными пользователя, например, их отобразить
-            console.log('Данные пользователя:', userData);
-          } else {
-            // Обработка ошибок при входе пользователя
-            console.error('Ошибка при входе:', Response.statusText);
-          }
-        } catch (error) {
-          console.error('Ошибка при выполнении запроса:', error);
+    })
+    .then(response => {
+        // Проверка успешности запроса на вход
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
         }
-      };
-
+        // Если запрос на вход выполнен успешно, отправляем запрос на /users/me
+        return fetch('https://553f-5-165-8-39.ngrok-free.app/users/me', {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json', // Установка Content-Type на application/json
+            },
+        });
+    })
+    .then(response => {
+        // Обработка ответа на запрос /users/me
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Преобразование ответа в JSON
+    })
+    .then(data => {
+        console.log(data); // Данные пользователя
+    })
+    .catch(error => {
+        console.error('There was a problem with your fetch operation:', error);
+    });
+}
 
     return (
         <form class="form-container" action="#" method="POST" onSubmit={handleSubmit(onSubmit)}>
             <h2>Форма авторизации</h2>
 
             <label for="username">Email:</label>
-            <input {...register("username")} type="email" id="username" name="username" required />
+            <input {...register("username")} type="email" value={username} onChange={(e) => setUsername(e.target.value)} id="username" name="username" required />
 
             <label for="password">Пароль:</label>
-            <input {...register("password")} type="password" id="password" name="password" required />
+            <input {...register("password")} type="password" value={password} onChange={(e) => setPassword(e.target.value)} id="password" name="password" required />
 
             <button type="submit">Войти</button>
         </form>
--- src/components/request/formRequest/Request.jsx
+++ src/components/request/formRequest/Request.jsx
@@ -2,17 +2,7 @@ import React, {Component} from 'react';
 import Userfront, { SignupForm } from "@userfront/toolkit/react";
 
 
-async function getInfo() {
-  const res = await window.fetch("/your-endpoint", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${Userfront.tokens.accessToken}`,
-    },
-  });
+function getInfo() {
 
-  console.log(res);
 }
-
-
 export default getInfo;
