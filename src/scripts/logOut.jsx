593daeb7cb2a46e53287d6cb439d893924a85405
Kostya Pershin <kostya.pershin.18@mail.ru>
2024-03-14 00:54:49 +0500

    logout update

--- src/scripts/logOut.jsx
+++ src/scripts/logOut.jsx
@@ -2,10 +2,23 @@ import React from 'react';
 
 export function LogoutComponent(){
     // Удаляем токен из локального хранилища
-    fetch('https://553f-5-165-8-39.ngrok-free.app/auth/logout', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json', // Установка Content-Type на application/json
-      }})
-      return
+    const logout = async () => {
+    try {
+        // Отправка запроса на выход пользователя
+        const response = await fetch('https://553f-5-165-8-39.ngrok-free.app/auth/logout', {
+            method: 'POST',
+            credentials: 'include', // Убедитесь, что куки прикрепляются к запросу
+        });
+
+        // Проверка успешности выполнения запроса на выход
+        if (response.ok) {
+            console.log('User logged out successfully');
+            // Выполните здесь необходимые действия после выхода пользователя (например, перенаправление на страницу входа)
+        } else {
+            console.error('Logout request failed');
+        }
+    } catch (error) {
+        console.error('Logout error:', error);
+    }
+}
 }
