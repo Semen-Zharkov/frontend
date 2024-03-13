4b079db4c70514a2120f3494deac1ed2b6756dd0
semen-zharkov <semen.zharkov.2017@yandex.ru>
2024-03-14 00:20:05 +0500

    update logOut

--- src/scripts/logOut.jsx
+++ src/scripts/logOut.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const LogoutComponent = () => {
+export function LogoutComponent(){
     // Удаляем токен из локального хранилища
     fetch('https://553f-5-165-8-39.ngrok-free.app/auth/logout', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json', // Установка Content-Type на application/json
-      }}
+      }})
+      return
 }
-export default LogoutComponent;
