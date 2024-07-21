128ec66da830843c7bc176d13cd8dc440c89aa6d
Semen-Zharkov <129843838+Semen-Zharkov@users.noreply.github.com>
2024-07-22 00:05:27 +0500

    изменены баги связанные с курсором

--- src/scripts/logOut.jsx
+++ src/scripts/logOut.jsx
@@ -1,8 +1,9 @@
+import { useNavigate } from "react-router-dom";
 const apiUrl = process.env.REACT_APP_API_URL;
-
+const navigate = useNavigate;
 export const logout = async () => {
-    
     try {
+        
         // Отправка запроса на выход пользователя
         const response = await fetch(`${apiUrl}/auth/logout`, {
             method: 'POST',
@@ -12,6 +13,8 @@ export const logout = async () => {
         if (response.ok) {
             document.location.reload();
             localStorage.clear()
+            navigate('/')
+
         } else {
             console.error('Logout request failed');
         }
